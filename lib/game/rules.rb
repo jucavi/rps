fb5e53272a68c6484f0ab43594782d8b27d9e8ff
jucavi <jcarvil@gmail.com>
2017-09-02 00:57:34 +0200

    Add Rules module

---
+++ lib/game/rules.rb
@@ -0,0 +1,18 @@
+#!/lib/rule.rb
+
+module Rules
+
+  RPS = {
+    scissors: [:rock],
+    rock: [:paper],
+    paper: [:scissors]
+  }
+
+  RPSSL = {
+    scissors: [:rock, :spock],
+    rock:   [:paper, :spock],
+    paper: [:scissors, :lizard],
+    spock: [:paper, :lizard],
+    lizard: [:scissors, :rock]
+  }
+end
